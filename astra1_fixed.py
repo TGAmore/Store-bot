8f953e74fa6083d5a221e6baa6c2b3399ad2f615
TGAmore <98864996+TGAmore@users.noreply.github.com>
2025-11-26 10:48:40 +0300

    Update astra1_fixed.py

--- astra1_fixed.py
+++ astra1_fixed.py
@@ -1,5 +1,3 @@
-from flask import Flask
-import threading
 import telebot
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -13,8 +11,8 @@ import time
 
 API_TOKEN = '7652837258:AAFsCZKdyfobBMz4KP1KGD6J3uUotHm-u7s'
 bot = telebot.TeleBot(API_TOKEN)
-app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 ADMIN_ID = 5584938116
 def get_connection():
     conn = sqlite3.connect(db_path, check_same_thread=False)
@@ -54,19 +52,15 @@ CREATE TABLE IF NOT EXISTS banned_users (
 )
 ''')
 conn.commit()
-def init_db():
-    conn, cur = get_connection()
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS offers (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            details TEXT,
-            price REAL,
-            quantity INTEGER
-        )
-    ''')
-    conn.commit()
-
+cursor.execute('''CREATE TABLE IF NOT EXISTS offers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    details TEXT NOT NULL,
+    price REAL NOT NULL,
+    quantity INTEGER NOT NULL,
+    image TEXT
+)''')
+conn.commit()
 cursor.execute('''
         CREATE TABLE IF NOT EXISTS recharge_requests (
             request_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -77,7 +71,6 @@ cursor.execute('''
         )
         ''')
 conn.commit()
-init_db()
 try:
     cursor.execute("ALTER TABLE offers ADD COLUMN category TEXT;")
     conn.commit()
@@ -882,16 +875,8 @@ def get_banned_users(message):
         bot.send_message(message.chat.id, f"قائمة المستخدمين المحظورين:\n{banned_users_list}" )
     else:
         bot.send_message(message.chat.id, "لا يوجد مستخدمين محظورين حتى الآن." )
-        
-@app.route('/')
-def home():
-    return "بوت شغال!"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=5000)
-
-flask_thread = threading.Thread(target=run_flask, daemon=True)
-flask_thread.start()
-
-# ------------------- تشغيل البوت -------------------
-bot.infinity_polling()
+        logging.basicConfig(level=logging.DEBUG)
+        logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+        bot.polling(none_stop=True, interval=0, timeout=20, long_polling_timeout=60)
+        time.sleep(15)
